d71008adf73b60226b2ba9a95396568a13233554
errusso <zucchinaberg69@gmail.com>
2024-07-04 22:40:12 +0200

    Better comment + implemented Halt

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,25 @@ fn main() {
     memory.init();
     cpu.init();
 
-    memory.load(0x0, 0x1); // mov
-    memory.load(0x1, 0x0); // a
-    memory.load(0x2, 0x3); // 0x3
-    memory.load(0x3, 0x1); // mov
-    memory.load(0x4, 0x1); // b
-    memory.load(0x5, 0x6); // 0x6
+    memory.load(0x0, 0x1); // mov a, 0x3
+    memory.load(0x1, 0x0);
+    memory.load(0x2, 0x3);
+    memory.load(0x3, 0x1); // mov bm, 0x6
+    memory.load(0x4, 0x1); 
+    memory.load(0x5, 0x6); 
     memory.load(0x6, 0x2); // add
-    memory.load(0x7, 0x3); // put
-    memory.load(0x8, 0xa); // 0xF
+    memory.load(0x7, 0x3); // put 0xa
+    memory.load(0x8, 0xa); 
+    memory.load(0x9, 0xF); // HLT
 
     memory.print_from_to(0, 0xb);
 
-    for i in 0../*128*/4{
+    for _ in 0..128{
         cpu.print_status();
+        if cpu.fetch_instruction(&mut memory) == 0xF{
+            println!("HLT");
+            break;
+        }
         cpu.run(&mut memory);
     }
 
