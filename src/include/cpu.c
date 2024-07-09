967ab648d545b4bd910003e6a90c215f3ca71324
ErRusso <zucchinaberg69@gmail.com>
2024-07-09 14:55:18 +0200

    added basic math and mem/reg management

--- src/include/cpu.c
+++ src/include/cpu.c
@@ -15,6 +15,7 @@ void cpu_print_status(cpu *c, memory *m)
 {
   printf("ip %d, code %d, [a %d,b %d,c %d, i %d]\n", c->ip, mem_get_code(m, c->ip), c->a, c->b, c->c, c->i);
 }
+
 void cpu_init(cpu *c)
 {
   c->a = 0;
@@ -34,11 +35,77 @@ void cpu_halt(cpu *c, memory *m)
   c->ip = 255;
 }
 
+void cpu_put(cpu *c, memory *m)
+{
+  int16_t dest = mem_get_code(m, c->ip + 1);
+  int16_t value = mem_get_code(m, c->ip + 2);
+  switch (dest)
+  {
+  case 'a':
+    c->a = value;
+    break;
+  case 'b':
+    c->b = value;
+    break;
+  case 'c':
+    c->c = value;
+    break;
+  case 'i':
+    c->i = value;
+    break;
+
+  default:
+    break;
+  }
+
+  c->ip += 2;
+}
+
+void cpu_mov(cpu *c, memory *m)
+{
+  int16_t dest = mem_get_code(m, c->ip + 1);
+  int16_t reg = mem_get_code(m, c->ip + 2);
+  int16_t value = reg == 'a' ? c->a : reg == 'b' ? c->b
+                                  : reg == 'c'   ? c->c
+                                                 : c->i;
+
+  mem_put_data(m, dest, value);
+
+  c->ip += 2;
+}
+
+void cpu_add(cpu *c, memory *m)
+{
+  c->a += c->b;
+}
+
+void cpu_sub(cpu *c, memory *m)
+{
+  c->a -= c->b;
+}
+
+void cpu_mul(cpu *c, memory *m)
+{
+  c->a *= c->b;
+}
+
+void cpu_div(cpu *c, memory *m)
+{
+  c->a /= c->b;
+  c->i = c->a % c->b;
+}
+
 void cpu_run(cpu *c, memory *m)
 {
   instruction instructions[256] = {
       &cpu_next,
       &cpu_halt,
+      &cpu_add,
+      &cpu_sub,
+      &cpu_mul,
+      &cpu_div,
+      &cpu_put,
+      &cpu_mov,
   };
 
   while (c->ip < 256)
